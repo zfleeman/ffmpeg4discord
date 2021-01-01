f7b5d6561f8a48ec3c04ea9ae1529349ccd31c26
Zach Fleeman <zfleeman@gmail.com>
2021-01-01 14:35:30 -0700

    chaning docker string in the batch file.

--- encode.bat
+++ encode.bat
@@ -3,5 +3,5 @@ For %%A in ("%filename%") do (
     Set Folder=%%~dpA
     Set Name=%%~nxA
 )
-docker run --rm -v %1:/usr/app/in/%Name% -v D:\shadowplay:/usr/app/out/ zachfleeman/ffmpeg4discord
+docker run --rm -v %1:/usr/app/in/%Name% -v D:\shadowplay:/usr/app/out/ zachfleeman/ffmpeg4discord:webm
 PAUSE
