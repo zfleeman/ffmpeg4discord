5cb7c73524916707925341cfd24c9b4c652de117
Zach Fleeman <zfleeman@gmail.com>
2023-01-07 22:51:59 -0700

    Adding cropping and changing the video filters.

--- discord.py
+++ discord.py
@@ -29,9 +29,29 @@ def time_calculations(fname, length):
 
     return duration, times
 
+def apply_video_filters(ffInput):
+    video = ffInput.video
+
+    if args.crop:
+        dim = args.crop.split('x')
+        video = video.crop(x=dim[0], y=dim[1], width=dim[2], height=dim[3])
+        args.inputratio = int(dim[2])/int(dim[3])
+   
+    if args.resolution:
+        video = video.filter('scale', args.resolution)
+        x = int(args.resolution.split('x')[0])
+        y = int(args.resolution.split('x')[1])
+        outputratio = x/y
+
+        if args.inputratio != outputratio:
+            print("!!!!!!!!!\n!WARNING!\n!!!!!!!!!")
+            print("Your output resolution's aspect ratio does not match the\ninput resolution's or your croped resolution's aspect ratio.")
+        
+    return video
+
 def first_pass(inputPath, params, times):
     ffInput = ffmpeg.input(inputPath, **times)
-    video = ffInput.video.filter('scale', args.resolution)
+    video = apply_video_filters(ffInput)
     ffOutput = ffmpeg.output(video, 'pipe:', **params)
     ffOutput = ffOutput.global_args('-loglevel', 'quiet', '-stats')
     std_out, std_err = ffOutput.run(capture_stdout=True)
@@ -39,8 +59,8 @@ def first_pass(inputPath, params, times):
 def second_pass(inputPath, outputPath, params, times):
     ffInput = ffmpeg.input(inputPath, **times)
     audio = ffInput.audio
-    video = ffInput.video.filter('scale', args.resolution)
-    ffOutput = ffmpeg.output(video, audio, outputPath,**params)
+    video = apply_video_filters(ffInput)
+    ffOutput = ffmpeg.output(video, audio, outputPath, **params)
     ffOutput = ffOutput.global_args('-loglevel', 'quiet', '-stats')
     ffOutput.run(overwrite_output=True)
 
@@ -52,16 +72,18 @@ parser = argparse.ArgumentParser(prog='ffmpeg4discord', description='Video compr
 parser.add_argument('filename', help='The full file path of the file that you wish to compress.')
 parser.add_argument('-o', '--output', default='', help='The desired output directory where the file will land.')
 parser.add_argument('-c', '--codec', choices=['libx264'], default='libx264', help='The codec that will be used during this conversion. libx264 is the most common and compatible codec.')
-parser.add_argument('-r', '--resolution', default='1280x720', help='The output resolution of your final video.')
 parser.add_argument('-s', '--filesize', default=8.0, type=float, help='The output file size in MB. Free Discord accepts a max of 8MB.')
 parser.add_argument('-a', '--audio-br', default=96, type=float, help='Audio bitrate in kbps.')
-# parser.add_argument('-x', '--crop', help="Cropping dimensions. Example: 255x0x1410x1080")
+# video filters
+parser.add_argument('-x', '--crop', help="Cropping dimensions. Example: 255x0x1410x1080")
+parser.add_argument('-r', '--resolution', help='The output resolution of your final video.')
 args = parser.parse_args()
 
 # pre-run variables
 fname = args.filename.replace("\\", "/").split('/')[-1]
 target_fs = args.filesize
 probe = ffmpeg.probe(args.filename)
+args.inputratio = probe['streams'][0]['width']/probe['streams'][0]['height']
 duration = math.floor(float(probe['format']['duration']))
 duration, times = time_calculations(fname, duration)
 run = True
@@ -92,7 +114,9 @@ while run:
         'bufsize': br * 2
     }
 
-    output_filename = args.output + 'small_' + datetime.strftime(datetime.now(), '%Y%m%d%H%M%S_') + fname
+    split_fname = fname.split('.')
+    output_filename = args.output + 'small_' + \
+        split_fname[0].replace(" ", "_") + datetime.strftime(datetime.now(), '_%Y%m%d%H%M%S.') + split_fname[1]
 
     print('Performing first pass.')
     first_pass(args.filename, pass_one_params, times)
