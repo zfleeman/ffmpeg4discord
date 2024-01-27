fa2d03eafff7ff2729f8cf4b21dfa5908f21332e
Zach Fleeman <zfleeman@gmail.com>
2024-01-26 20:23:55 -0700

    revised windows script and added readme

--- windows_setup.py
+++ windows_setup.py
@@ -1,40 +1,36 @@
+# I know this is sloppy and kind of sucks, but I figured it would help installation at least a little bit.
+
 from urllib.request import urlretrieve
 import zipfile
 import subprocess
 import os
+import pyperclip
+
+# Get the current system-wide PATH
+current_path = os.environ.get("PATH", "")
+
+if "ffmpeg" in current_path:
+    raise SystemExit("ffmpeg is already present in your system PATH!")
 
 curdir = os.getcwd()
 
 print("Downloading ffmpeg to ffmpeg.zip...")
 urlretrieve("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip", "ffmpeg.zip")
-print("Done!")
+print("Done!\n")
 
 print("Unzipping ffmpeg.zip...")
-with zipfile.ZipFile("ffmpeg.zip", 'r') as zip_ref:
+with zipfile.ZipFile("ffmpeg.zip", "r") as zip_ref:
     zip_ref.extractall()
     folder = f"{curdir}\\{zip_ref.namelist()[0][:-1]}\\bin"
-print("Done!")
+print("Done!\n")
 
-def add_to_system_path(directory) -> None:
-    # Get the current system-wide PATH
-    current_path = os.environ.get('PATH', '')
+pyperclip.copy(folder)
+print(f"'{folder}' has been copied to your clipboard.\n")
 
-    if "ffmpeg" in current_path:
-        print("ffmpeg is already present in your system PATH!")
-        return
+print("Launching environment variable editor.")
+print("Add the copied ffmpeg folder path as an entry to the already-existing Path variable in the editor.")
+print("Click the 'Ok' buttons to exit the dialog after you're done.")
 
-    # Append the new directory to the PATH, separated by a semicolon
-    new_path = f"{current_path};{directory}"
+subprocess.run(["rundll32", "sysdm.cpl,EditEnvironmentVariables"], check=True)
 
-    # Set the modified PATH variable for the current session
-    os.environ['PATH'] = new_path
-
-    try:
-        # Use setx command to set the PATH persistently
-        subprocess.run(['setx', 'PATH', new_path], check=True)
-        print(f"Added {directory} to the system-wide PATH permanently.")
-    except subprocess.CalledProcessError as e:
-        print(f"Failed to set the PATH permanently: {e}")
-
-directory_to_add = folder
-add_to_system_path(directory_to_add)
+print("\nOpen a new Terminal window and type ffmpeg and hit enter to verify that the installation succeeded.")
