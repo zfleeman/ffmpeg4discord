cfc2aed8026e3caee42ccc501338fb13f031509b
Zach Fleeman <zfleeman@gmail.com>
2024-02-08 23:19:56 -0700

    reworking windows installation

--- ffmpeg4discord/windows.py
+++ ffmpeg4discord/windows.py
@@ -1,38 +1,56 @@
 # I know this is sloppy and kind of sucks, but I figured it would help installation at least a little bit.
 
+import sys
 from urllib.request import urlretrieve
 import zipfile
-import subprocess
-import os
-import pyperclip
+from pathlib import Path
+import shutil
+
+target = Path(sys.executable).parent
+
+
+def copy_directory_contents(source, target):
+    source_path = Path(source)
+    target_path = Path(target)
+
+    # Iterate over files in the source directory
+    for item in source_path.iterdir():
+        # Construct target item path
+        target_item = target_path / item.name
+
+        # If the item is a file, copy it to the target directory
+        if item.is_file():
+            shutil.copy2(item, target_item)
+            print(f"Copied file: {item} -> {target_item}")
+
+
+def download_with_progress(url, save_path):
+    def report(block_num, block_size, total_size):
+        downloaded = block_num * block_size
+        progress = min(downloaded / total_size, 1.0)
+        percent = round(progress * 100, 2)
+        print(f"\rDownloaded {downloaded}/{total_size} bytes ({percent}%)", end="")
+
+    urlretrieve(url, save_path, reporthook=report)
+    print("\nDownload complete!")
 
 
 def setup():
-    # Get the current system-wide PATH
-    current_path = os.environ.get("PATH", "")
-
-    if "ffmpeg" in current_path:
-        raise SystemExit("ffmpeg is already present in your system PATH!")
-
-    curdir = os.getcwd()
 
     print("Downloading ffmpeg to ffmpeg.zip...")
-    urlretrieve("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip", "ffmpeg.zip")
-    print("Done!\n")
+    download_with_progress("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip", "ffmpeg.zip")
 
     print("Unzipping ffmpeg.zip...")
     with zipfile.ZipFile("ffmpeg.zip", "r") as zip_ref:
         zip_ref.extractall()
-        folder = f"{curdir}\\{zip_ref.namelist()[0][:-1]}\\bin"
+        source = Path(f"{zip_ref.namelist()[0]}/bin").resolve()
     print("Done!\n")
 
-    pyperclip.copy(folder)
-    print(f"'{folder}' has been copied to your clipboard.\n")
+    copy_directory_contents(source=source, target=target)
 
-    print("Launching environment variable editor.")
-    print("Add the copied ffmpeg folder path as an entry to the already-existing Path variable in the editor.")
-    print("Click the 'Ok' buttons to exit the dialog after you're done.")
-
-    subprocess.run(["rundll32", "sysdm.cpl,EditEnvironmentVariables"], check=True)
-
+    print("\nffmpeg installation complete.")
     print("\nOpen a new Terminal window and type ffmpeg and hit enter to verify that the installation succeeded.")
+
+
+if __name__ == "__main__":
+    setup()
