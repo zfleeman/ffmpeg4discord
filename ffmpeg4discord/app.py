f2eb00ad65a374c75d5853ae4729c112c1c1e90a
Zach Fleeman <zfleeman@gmail.com>
2024-02-25 15:48:51 -0700

    port improvments after user feedback

--- ffmpeg4discord/app.py
+++ ffmpeg4discord/app.py
@@ -14,7 +14,10 @@ from ffmpeg4discord.twopass import TwoPass
 # get args from the command line
 args = arguments.get_args()
 web = args.pop("web")
-port = args.pop("port")
+
+if web:
+    port = args.pop("port")
+
 path = Path(args["filename"]).resolve()
 args["filename"] = path
 
@@ -22,7 +25,7 @@ args["filename"] = path
 twopass = TwoPass(**args)
 
 
-def twopass_loop(target_filesize: float) -> str:
+def twopass_loop(target_filesize: float) -> None:
     while twopass.run() >= target_filesize:
         print(
             f"\nThe output file size ({round(twopass.output_filesize, 2)}MB) is still above the target of {target_filesize}MB.\nRestarting...\n"
@@ -32,10 +35,7 @@ def twopass_loop(target_filesize: float) -> str:
         # adjust the class's target file size to set a lower bitrate for the next run
         twopass.target_filesize -= 0.2
 
-    success_message = f"Your compressed video file ({round(twopass.output_filesize, 2)}MB) is located at {Path(twopass.output_filename).resolve()}"
-    print(success_message)
-
-    return success_message
+    twopass.message = f"Your compressed video file ({round(twopass.output_filesize, 2)}MB) is located at {Path(twopass.output_filename).resolve()}"
 
 
 def seconds_to_timestamp(seconds: int) -> str:
@@ -88,13 +88,13 @@ def main() -> None:
                 "web.html",
                 file_url=url_for("static", filename=path.name),
                 twopass=twopass,
-                alert=success_message,
             )
 
         threading.Thread(target=open_browser, name="Open Browser").start()
         app.run("0.0.0.0", port=port)
     else:
         twopass_loop(twopass.target_filesize)
+        print(twopass.message)
 
 
 if __name__ == "__main__":
